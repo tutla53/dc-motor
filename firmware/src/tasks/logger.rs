d9df495330cd109d9b9cdf858e42982a569d53ee
tutla53 <ayatullah_tutla@yahoo.com>
2026-02-04 15:45:26 +0700

    Change Mutex to Atomic

--- firmware/src/tasks/logger.rs
+++ firmware/src/tasks/logger.rs
@@ -23,16 +23,16 @@ pub async fn firmware_logger_task() {
     let mut sequence: u8 = 0;
 
     loop {
-        if LOGGER.is_logging_active().await {
+        if LOGGER.is_logging_active() {
             let data = LogData {
                 seq: sequence,
                 dt: start.elapsed().as_millis() as u32,
                 values : [
-                    MOTOR_0.get_current_pos().await,
-                    MOTOR_0.get_current_speed().await,
-                    MOTOR_0.get_commanded_pos().await,
-                    MOTOR_0.get_commanded_speed().await,
-                    MOTOR_0.get_commanded_pwm().await,
+                    MOTOR_0.get_current_pos(),
+                    MOTOR_0.get_current_speed(),
+                    MOTOR_0.get_commanded_pos(),
+                    MOTOR_0.get_commanded_speed(),
+                    MOTOR_0.get_commanded_pwm(),
                 ],
             };
 
@@ -43,7 +43,7 @@ pub async fn firmware_logger_task() {
         else {
             Timer::after(Duration::from_millis(100)).await;
             start = Instant::now();
-            let new_time_sampling = LOGGER.get_logging_time_sampling().await;
+            let new_time_sampling = LOGGER.get_logging_time_sampling();
             ticker = Ticker::every(Duration::from_millis(new_time_sampling));
             LOGGER.log_tx_buffer.clear();
             ticker.reset();
