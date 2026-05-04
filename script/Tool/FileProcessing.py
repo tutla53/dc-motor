542eacab24798e22debfe61b729cd148ed4f1202
tutla53 <ayatullah_tutla@yahoo.com>
2026-05-04 16:47:23 +0700

    Create the Tool to extract firmware log data from csv file

---
+++ script/Tool/FileProcessing.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+
+def extract_firmware_log_data(log_path):
+    print(log_path)
+    data = pd.read_csv(log_path)
+    dt_ms = data["Timestamp(ms)"][1] - data["Timestamp(ms)"][0]
+    dt_s = dt_ms/1000.0
+    t = data["Timestamp(ms)"]/1000.0
+    actual_speed = data["Motor_Speed(RPM)"]
+    commanded = data["Commanded_Speed(RPM)"]
+    
+    target = np.max(np.abs(commanded))
+    duration = len(t)
+    
+    start_time = 0
+    initial_pos = commanded[0]
+    RPM_TRESHOLD = 10
+    for i in range(1, len(t)):
+        if np.abs(commanded[i]-initial_pos) > RPM_TRESHOLD:
+            start_time = i
+            break
+            
+    return (target, (start_time*dt_s), (duration*dt_s), dt_s, commanded, actual_speed, t)
