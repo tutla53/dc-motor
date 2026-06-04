a1d903c8d8c025481534d114a628e34064016c33
tutla53 <ayatullah_tutla@yahoo.com>
2026-06-04 16:01:29 +0700

    Updating System Config

--- script/Config/Motor0.py
+++ script/Config/Motor0.py
@@ -9,8 +9,8 @@ motor_id = 0
 GEAR_RATIO              = 4.4
 ENCODER_PPR             = 11
 ROTATION_PER_PULSE      = 1/(GEAR_RATIO*ENCODER_PPR)
-MAX_SPEED_RPS           = 807
-MAX_SPEED_RPM           = 1000
+MAX_SPEED_PPS           = 887
+MAX_SPEED_RPM           = MAX_SPEED_PPS * ROTATION_PER_PULSE * 60
 
 # Electronic Properties
 SYSTEM_FREQ_HZ          = 133_000_000
@@ -22,13 +22,13 @@ FREQUENCY_SAMPLING_HZ   = 1000
 DT_S                    = 1/FREQUENCY_SAMPLING_HZ
 
 ## Linear Model Properties
-K                       = 0.26190  # (pulse per seconds)/PWM_TICKS
-TAU_S                   = 0.02664   # seconds
-DELAY_TIME_S            = 0.01461   # seconds
+K                       = 0.27   # (pulse per seconds)/PWM_TICKS
+TAU_S                   = 0.0263   # seconds
+DELAY_TIME_S            = 0.014     # seconds
 DELAY_STEPS             = int(DELAY_TIME_S / DT_S)
 
 ## Nonlinear Model Properties
-data = pd.read_csv(base_url+"/Config/system_identification.csv")
-PWM_LIST = data["PWM"].values
-K_LIST = data["K"].values
-TAU_LIST = data["tau"].values
+data        = pd.read_csv(base_url+"/Config/system_identification.csv")
+PWM_LIST    = data["PWM"].values
+K_LIST      = data["K"].values
+TAU_LIST    = data["tau"].values
