fb342bce47212d575e91e1f9162d5f7e69827148
tutla53 <ayatullah_tutla@yahoo.com>
2026-06-20 18:52:20 +0700

    Integrate the open loop, speed control, and position control on the MoveMotor

--- script/BasicFunction/Motor.py
+++ script/BasicFunction/Motor.py
@@ -1,12 +1,16 @@
 import time
 import math
 import numpy as np
+import os
 import FWLogger.FWLogger as Logger
 import Tool.SystemIdentification
 import Tool.MotorSim
+import Tool.FileProcessing
+import Tool.plotter
+from base_url import *
 
-from scipy.signal import lfilter
 from Tool.visualize import *
+from Tool.MotorSim import SystemModel
 
 class MoveMotor:
     def __init__(self, device, configfile):
@@ -136,3 +140,190 @@ class MoveMotor:
         
         return t_total
     
+    def move_open_loop(self, pwm, duration=1.0, time_sampling = 1, mode: SystemModel = SystemModel.Nonlinear):
+        #############################################################################
+        # Directory
+        group_name      = "Open_Loop_"
+        tag = group_name + time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
+        
+        # File Properties
+        command_header  = "Commanded_PWM"
+        data_header     = "Motor_Speed(RPM)"
+        
+        # Plot Config
+        plot_title          = "Open Loop Response"
+        y_label             = "Motor Speed(RPM)"
+        data_legend         = "Actual Open Loop Response"
+        commanded_legend    = "PWM Input"
+        simulation_legend   = "Open Loop Simulation"
+        same_unit           = False
+        #############################################################################
+        
+        self.start_log(mask = 18, time_sampling=time_sampling, folder_tag=tag)
+        self.move_motor_open_loop(pwm)
+        time.sleep(duration)
+        self.stop_motor()
+        dir_name = self.stop_log()
+        
+        log_dir = base_url+"/LOG/"+dir_name+"/"
+        filename = os.listdir(base_url+"/LOG/"+dir_name)[0]
+    
+        motor_response = Tool.FileProcessing.extract_firmware_log_data(log_dir+filename, command_header, data_header, threshold_percent=2.0)
+        
+        if motor_response["start_time_s"] is not None:
+            simulation_time_list, simulation_speed, simulation_target = self.sim.simulate_open_loop_response(
+                                                                    target_pwm  = pwm,
+                                                                    u           = motor_response["command_list"],
+                                                                    mode        = mode,
+                                                                    )
+            
+            Tool.plotter.create_simulation_plot(log_dir                     = log_dir, 
+                                                pid_config                  = None,
+                                                simulation_time_list        = simulation_time_list, 
+                                                actual_time_list            = motor_response["time_list"],
+                                                simulation_data_list        = simulation_speed, 
+                                                simulation_commanded_list   = simulation_target,  
+                                                actual_data_list            = motor_response["data_list"], 
+                                                actual_commanded_list       = motor_response["command_list"],
+                                                tag                         = group_name,
+                                                plot_title                  = plot_title,
+                                                y_label                     = y_label,
+                                                actual_data_label           = data_legend,
+                                                actual_commanded_label      = commanded_legend,
+                                                simulation_data_label       = simulation_legend,
+                                                simulation_commanded_label  = commanded_legend,
+                                                same_unit                   = same_unit, 
+                                                )
+
+    def move_speed(self, speed_rpm, time_sampling = 1, mode: SystemModel = SystemModel.Nonlinear):
+        #############################################################################
+        # Directory
+        group_name      = "Speed_Step_"
+        tag = group_name + time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
+        
+        # File Properties
+        command_header  = "Commanded_Speed(RPM)"
+        data_header     = "Motor_Speed(RPM)"
+        
+        # Plot Config
+        plot_title          = "Speed Control Response"
+        y_label             = "Motor Speed(RPM)"
+        data_legend         = "Actual Motor Response"
+        commanded_legend    = "Commanded Speed"
+        simulation_legend   = "Speed Simulation"
+        same_unit           = True
+        #############################################################################
+        
+        pid_config = self.dev.get_pid_motor_speed(self.motor_id)
+    
+        kp = pid_config["kp"]
+        ki = pid_config["ki"]
+        kd = pid_config["kd"]
+        i_limit = pid_config["i_limit"]
+        
+        pid_params = [kp, ki, kd, i_limit]
+        
+        self.start_log(mask=10, time_sampling=time_sampling, folder_tag=tag)
+        self.move_motor_speed(speed_rpm)
+        time.sleep(1)
+        self.stop_motor()
+        dir_name = self.stop_log()
+        
+        log_dir = base_url+"/LOG/"+dir_name+"/"
+        filename = os.listdir(base_url+"/LOG/"+dir_name)[0]
+    
+        motor_response = Tool.FileProcessing.extract_firmware_log_data(log_dir+filename, command_header, data_header, threshold_percent=2.0)
+        
+        if motor_response["start_time_s"] is not None:
+            simulation_time_list, simulation_speed, simulation_target = self.sim.simulate_pid_speed_control(
+                                                            pid_params      = pid_params,
+                                                            initial_speed   = motor_response["command_list"][0]/self.config.ROTATION_PER_PULSE/60,
+                                                            setpoint        = motor_response["command_list"]/self.config.ROTATION_PER_PULSE/60,
+                                                            mode            = mode
+                                                            )
+            
+            Tool.plotter.create_simulation_plot(log_dir                     = log_dir, 
+                                                pid_config                  = pid_params,
+                                                simulation_time_list        = simulation_time_list, 
+                                                actual_time_list            = motor_response["time_list"],
+                                                simulation_data_list        = simulation_speed, 
+                                                simulation_commanded_list   = simulation_target,  
+                                                actual_data_list            = motor_response["data_list"], 
+                                                actual_commanded_list       = motor_response["command_list"],
+                                                tag                         = group_name,
+                                                plot_title                  = plot_title,
+                                                y_label                     = y_label,
+                                                actual_data_label           = data_legend,
+                                                actual_commanded_label      = commanded_legend,
+                                                simulation_data_label       = simulation_legend,
+                                                simulation_commanded_label  = commanded_legend,
+                                                same_unit                   = same_unit, 
+                                                )
+
+    def move_pos(self, position_rotation, speed_rpm=None, acc_rpm=None, duration=1.5, time_sampling = 1, mode: SystemModel = SystemModel.Nonlinear):
+        #############################################################################
+        # Directory
+        group_name      = "Position_Step_"
+        tag = group_name + time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
+        
+        # File Properties
+        command_header  = "Commanded_Position(rotation)"
+        data_header     = "Motor_Position(rotation)"
+        
+        # Plot Config
+        plot_title          = "Position Control Response"
+        y_label             = "Motor Position (Rotation)"
+        data_legend         = "Actual Motor Response"
+        commanded_legend     = "Commanded Position"
+        simulation_legend   = "Position Simulation"
+        same_unit           = True
+        #############################################################################
+        
+        pid_speed = self.dev.get_pid_motor_speed(self.motor_id)
+        pid_speed_config = [pid_speed["kp"], pid_speed["ki"], pid_speed["kd"], pid_speed["i_limit"]]
+        
+        pid_pos = self.dev.get_pid_motor_pos(self.motor_id)
+        pid_pos_config = [pid_pos["kp"], pid_pos["ki"], pid_pos["kd"], pid_pos["i_limit"]] 
+        
+        self.start_log(mask = 5, time_sampling=time_sampling, folder_tag=tag)
+        
+        if speed_rpm == None or acc_rpm == None:
+            self.move_motor_pos_step(position_rotation, print_motor_log=True)
+        else:
+            self.move_motor_pos_trapezoid(position_rotation, speed_rpm=speed_rpm, acc_rpm=acc_rpm, print_motor_log=True)
+        time.sleep(duration)
+        self.stop_motor()
+        dir_name = self.stop_log()
+        
+        log_dir = base_url+"/LOG/"+dir_name+"/"
+        filename = os.listdir(base_url+"/LOG/"+dir_name)[0]
+    
+        motor_response = Tool.FileProcessing.extract_firmware_log_data(log_dir+filename, command_header, data_header, threshold_percent=2.0)
+        
+        initial_position = motor_response["command_list"][0]
+        
+        simulation_time_list, simulation_pos, simulation_target, simulation_speed = self.sim.simulate_pid_pos_control(
+                                                                pid_pos_params      = pid_pos_config,
+                                                                pid_speed_params    = pid_speed_config,
+                                                                initial_pos         = initial_position/self.config.ROTATION_PER_PULSE,
+                                                                setpoint            = motor_response["command_list"]/self.config.ROTATION_PER_PULSE,
+                                                                mode                = mode,
+                                                                )
+        
+        Tool.plotter.create_simulation_plot(log_dir                     = log_dir, 
+                                            pid_config                  = pid_pos_config,
+                                            simulation_time_list        = simulation_time_list, 
+                                            actual_time_list            = motor_response["time_list"],
+                                            simulation_data_list        = simulation_pos, 
+                                            simulation_commanded_list   = simulation_target,  
+                                            actual_data_list            = motor_response["data_list"], 
+                                            actual_commanded_list       = None,
+                                            tag                         = group_name,
+                                            plot_title                  = plot_title,
+                                            y_label                     = y_label,
+                                            actual_data_label           = data_legend,
+                                            actual_commanded_label      = commanded_legend,
+                                            simulation_data_label       = simulation_legend,
+                                            simulation_commanded_label  = commanded_legend,
+                                            same_unit                   = same_unit, 
+                                            )                 
