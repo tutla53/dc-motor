073f2def787fdc5d9cabadd34199159f10915691
tutla53 <ayatullah_tutla@yahoo.com>
2026-07-08 18:30:15 +0700

    Creating move_motor_pos_trapezoid example based on the changes

--- rust_script/src/main.rs
+++ rust_script/src/main.rs
@@ -1,12 +1,16 @@
 mod board;
 mod basic_function;
 mod logger;
+mod config;
 
 use crate::board::rpi::Pico;
 use crate::basic_function::move_motor::Motor;
+use crate::basic_function::conversion::Position;
+use crate::basic_function::conversion::Speed;
+use crate::basic_function::conversion::Acceleration;
 use crate::logger::fwlogger::Logger;
+use crate::basic_function::utility::wait_ms;
 
-use std::thread;
 use std::time::Duration;
 use std::sync::{Arc, Mutex};
 
@@ -15,33 +19,37 @@ include!(concat!(env!("OUT_DIR"), "/generated_commands.rs"));
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (pico, log_rx) = Pico::new(env!("CONFIG_FILE"))?;
     let shared_pico = Arc::new(Mutex::new(pico));
-
-    if let Ok(mut pico) = shared_pico.lock() {
-        match pico.get_firmware_version() {
-            Ok((major, minor, patch)) => println!("Firmware: {}.{}.{}", major, minor, patch),
-            Err(e) => println!("Error: {}", e),
-        }
-    }
     
-    let m0 = Motor::new(Arc::clone(&shared_pico), 0);
-    let mut logger = Logger::new(Arc::clone(&shared_pico), log_rx);
+    let motor_id = 0;
+    let m0 = Motor::new(Arc::clone(&shared_pico), motor_id);
+    let mut logger = Logger::new(Arc::clone(&shared_pico), log_rx, m0.motor_id);
+
+    let current_pos = m0.get_motor_pos()?;
+    println!("Initial Pos: {} count, {:.2} rotation", current_pos.count, current_pos.rotation);
+
+    let target_rotation = if current_pos.rotation != 0.0 { 0.0 } else { 20.0 };
+    let log_mask = 5;
+    let time_sampling = 1;
 
     m0.clear_motor_event();
-    logger.start(5, 1);
-    thread::sleep(Duration::from_millis(500));
 
-    m0.move_motor_trapezoid(10_000.0, 1000.0, 10_000.0);
-    m0.wait_move_done(Duration::from_secs(20))?;
+    logger.start(log_mask, time_sampling);
+    wait_ms(300);
 
-    thread::sleep(Duration::from_millis(500));
-    logger.stop();
+    m0.move_motor_pos_trapezoid(
+        Position::from_rotation(target_rotation), 
+        Speed::from_rpm(1000.0), 
+        Acceleration::from_cps_sq(10_000),
+    );
     
-    if let Ok(mut pico) = shared_pico.lock() {
-        match pico.get_motor_pos(0) {
-            Ok(value) => println!("Final Motor Pos = {}", value),
-            Err(e) => println!("Error: {}", e),
-        }
-    }
+    m0.wait_move_done(Duration::from_secs(20))?;
+    
+    wait_ms(300);
+    logger.stop();
+    m0.stop_motor();
 
+    let current_pos = m0.get_motor_pos()?;
+    println!("Final Pos: {} count, {:.2} rotation", current_pos.count, current_pos.rotation);
+    
     Ok(())
 }
