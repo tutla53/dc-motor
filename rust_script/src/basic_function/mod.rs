be552774a2a7eb803221887ca61d3ddaafe52f36
tutla53 <ayatullah_tutla@yahoo.com>
2026-07-08 18:26:49 +0700

    Completing the basic motor move function

--- rust_script/src/basic_function/mod.rs
+++ rust_script/src/basic_function/mod.rs
@@ -1,8 +1,15 @@
 
 use crate::board::rpi::Pico;
+use crate::config::motor_config;
 
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::{Duration, Instant};
 
 pub mod move_motor;
+pub mod conversion;
+pub mod utility;
+
+use crate::basic_function::conversion::Position;
+use crate::basic_function::conversion::Speed;
+use crate::basic_function::conversion::Acceleration;
--- rust_script/src/basic_function/move_motor.rs
+++ rust_script/src/basic_function/move_motor.rs
@@ -3,9 +3,10 @@ use super::*;
 
 pub struct Motor {
     pico: Arc<Mutex<Pico>>,
-    motor_id: u8,
+    pub motor_id: u8,
 }
 
+#[allow(unused)]
 impl Motor {
     pub fn new(pico: Arc<Mutex<Pico>>, motor_id: u8) -> Self {
         Self {
@@ -13,11 +14,53 @@ impl Motor {
             motor_id,
         }
     }
-
-    pub fn move_motor_trapezoid(&self, target:f32, speed:f32, acc:f32) {
+    
+    pub fn stop_motor(&self) {
         if let Ok(mut pico) = self.pico.lock() {
-            let _ = pico.move_motor_abs_pos_trapezoid(self.motor_id, target, speed, acc);
+            let _ = pico.stop_motor(self.motor_id);
         }
+    }
+
+    pub fn move_motor_speed(&self, speed: Speed) {
+        if let Ok(mut pico) = self.pico.lock() {
+            let _ = pico.move_motor_speed(self.motor_id, speed.cps);
+        }
+    }
+
+    pub fn move_motor_pos_step(&self, target: Position,) {
+        if let Ok(mut pico) = self.pico.lock() {
+            let _ = pico.move_motor_abs_pos(self.motor_id, target.count);
+        }
+    }
+
+    pub fn move_motor_pos_trapezoid(&self, target: Position, speed: Speed, acc: Acceleration) {
+        if let Ok(mut pico) = self.pico.lock() {
+            let _ = pico.move_motor_abs_pos_trapezoid(self.motor_id, target.count, speed.cps, acc.cps_square);
+        }
+    }
+
+    pub fn move_motor_open_loop(&self, pwm: i32) {
+        if let Ok(mut pico) = self.pico.lock() {
+            let _ = pico.move_motor_open_loop(self.motor_id, pwm);
+        }
+    }    
+
+    pub fn get_motor_pos(&self) -> Result<Position, String> {
+        if let Ok(mut pico) = self.pico.lock() {            
+            if let Ok(count) = pico.get_motor_pos(self.motor_id) {
+                return Ok(Position::from_count(count));
+            }
+        }
+        return Err("Pico returned a runtime error flag for this command".to_string());
+    }
+
+    pub fn get_motor_speed(&self) -> Result<Speed, String> {
+        if let Ok(mut pico) = self.pico.lock() {            
+            if let Ok(cps) = pico.get_motor_speed(self.motor_id) {
+                return Ok(Speed::from_cps(cps));
+            }
+        }
+        return Err("Pico returned a runtime error flag for this command".to_string());
     }
 
     pub fn clear_motor_event(&self) {
