5fb641f1b154fb7baa3c800a5cb85b61c2deb873
tutla53 <ayatullah_tutla@yahoo.com>
2026-07-08 18:29:35 +0700

    Refactoring the logger and add double active logger handler

--- rust_script/src/logger/fwlogger.rs
+++ rust_script/src/logger/fwlogger.rs
@@ -1,131 +1,133 @@
 
+#![allow(unused)]
+
 use super::*;
-
-const N_ENUM: usize = 5;
-
-const LOG_MASK_NAMES: [&str; N_ENUM] = [
-    "Motor_Position(rotation)",
-    "Motor_Speed(RPM)",
-    "Commanded_Position(rotation)",
-    "Commanded_Speed(RPM)",
-    "Commanded_PWM",
-];
-
-const GEAR_RATIO: f64           = 4.4;
-const ENCODER_PPR: f64          = 11.0;
-const ROTATION_PER_PULSE: f64   = 1.0/(GEAR_RATIO*ENCODER_PPR);
-
-const SCALE_OFFSET_MOTOR: [[f64; 2]; N_ENUM] = [
-    [ROTATION_PER_PULSE, 0.0],
-    [60.0 * ROTATION_PER_PULSE, 0.0],
-    [ROTATION_PER_PULSE, 0.0],
-    [60.0 * ROTATION_PER_PULSE, 0.0],
-    [1.0, 0.0],
-];
 
 pub struct Logger {
     pico: Arc<Mutex<Pico>>,
     log_rx: Option<Receiver<LogEntry>>, 
     mask: i32,
-    collected_logs: Arc<Mutex<Vec<LogEntry>>>, 
+    collected_logs: Arc<Mutex<Vec<LogEntry>>>,
+    motor_id: u8,
+    is_logging_start: bool, 
 }
 
 impl Logger {
-    pub fn new(pico: Arc<Mutex<Pico>>, log_rx: Receiver<LogEntry>) -> Self {
+    pub fn new(pico: Arc<Mutex<Pico>>, log_rx: Receiver<LogEntry>, motor_id:u8) -> Self {
         Self { 
             pico,
             log_rx: Some(log_rx), 
             mask: 0,
             collected_logs: Arc::new(Mutex::new(Vec::new())),
+            motor_id,
+            is_logging_start: false,
         }
     }
 
     pub fn start(&mut self, mask: i32, sampling_rate_ms: u64) {
-        self.mask = mask;
 
-        if let Some(rx) = self.log_rx.take() {
-            let logs_clone = Arc::clone(&self.collected_logs);
-            thread::spawn(move || {
-                let mut local_buffer = Vec::with_capacity(100);
-                let mut last_flush = std::time::Instant::now();
+        if self.is_logging_start {
+            println!("  [WARN] - {}", "FW Logger is already started".bright_red().bold());
+        }
+        else {
+            self.is_logging_start = true;
+            self.mask = mask;
 
-                while let Ok(entry) = rx.recv() {
-                    local_buffer.push(entry);
+            if let Some(rx) = self.log_rx.take() {
+                let logs_clone = Arc::clone(&self.collected_logs);
+                thread::spawn(move || {
+                    let mut local_buffer = Vec::with_capacity(100);
+                    let mut last_flush = std::time::Instant::now();
 
-                    if last_flush.elapsed().as_millis() >= 50 || local_buffer.len() >= 100 {
+                    while let Ok(entry) = rx.recv() {
+                        local_buffer.push(entry);
+
+                        if last_flush.elapsed().as_millis() >= 50 || local_buffer.len() >= 100 {
+                            if let Ok(mut logs) = logs_clone.lock() {
+                                logs.append(&mut local_buffer);
+                            }
+                            last_flush = std::time::Instant::now();
+                        }
+                    }
+
+                    if !local_buffer.is_empty() {
                         if let Ok(mut logs) = logs_clone.lock() {
                             logs.append(&mut local_buffer);
                         }
-                        last_flush = std::time::Instant::now();
                     }
-                }
+                });
+            }
 
-                if !local_buffer.is_empty() {
-                    if let Ok(mut logs) = logs_clone.lock() {
-                        logs.append(&mut local_buffer);
-                    }
-                }
-            });
-        }
-
-        if let Ok(mut pico) = self.pico.lock() {
-            let _ = pico.start_logger(0, sampling_rate_ms);
+            if let Ok(mut pico) = self.pico.lock() {
+                let _ = pico.start_logger(self.motor_id, sampling_rate_ms);
+            }
         }
     }
 
-    pub fn stop(&self) {
-        if let Ok(mut pico) = self.pico.lock() {
-            let _ = pico.stop_logger(0);
+    pub fn stop(&mut self) {
+        if !self.is_logging_start {
+            println!("  [WARN] - {}", "FW Logger has not been started".bright_red().bold());
         }
+        else {
+            self.is_logging_start = false;
 
-        let collected_logs = match self.collected_logs.lock() {
-            Ok(mut logs) => std::mem::take(&mut *logs),
-            Err(_) => Vec::new(),
-        };
-
-        let folder_tag: &str = "TrapezoidRun";
-        let now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
-        let tag = format!("{}", now);
-        
-        let log_dir = if folder_tag.is_empty() {
-            format!("LOG/{}", tag)
-        } else {
-            format!("LOG/{}", folder_tag)
-        };
-
-        let _ = fs::create_dir_all(&log_dir);
-        let file_path = format!("{}/log_{}.csv", log_dir, tag);
-
-        let mut selected_data = Vec::new();
-        let mut columns = vec!["Timestamp(ms)".to_string()];
-
-        for bit in 0..N_ENUM {
-            let flag = 1 << bit;
-            if (self.mask & flag) != 0 {
-                selected_data.push(bit);
-                columns.push(LOG_MASK_NAMES[bit].to_string());
+            if let Ok(mut pico) = self.pico.lock() {
+                let _ = pico.stop_logger(self.motor_id);
             }
-        }
 
-        let file = fs::File::create(&file_path).unwrap();
-        let mut writer = csv::Writer::from_writer(file);
-        let _ = writer.write_record(&columns);
+            let collected_logs = match self.collected_logs.lock() {
+                Ok(mut logs) => std::mem::take(&mut *logs),
+                Err(_) => Vec::new(),
+            };
 
-        for data_line in collected_logs {
-            let mut row = vec![data_line.dt.to_string()];
+            if collected_logs.is_empty() {
+                println!("{}", "No Data Collected".bright_red().bold());
+                return
+            }
+            let folder_tag: &str = "TrapezoidRun";
+            let now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
+            let tag = format!("{}", now);
+            
+            let log_dir = if folder_tag.is_empty() {
+                format!("LOG/{}", tag)
+            } else {
+                format!("LOG/{}", folder_tag)
+            };
 
-            for &idx in &selected_data {
-                if idx < data_line.values.len() {
-                    let scale = SCALE_OFFSET_MOTOR[idx][0];
-                    let offset = SCALE_OFFSET_MOTOR[idx][1]; 
-                    let processed_value = (data_line.values[idx] as f64 * scale) + offset;
-                    row.push(format!("{:.4}", processed_value));
+            let _ = fs::create_dir_all(&log_dir);
+            let file_path = format!("{}/log_{}.csv", log_dir, tag);
+
+            let mut selected_data = Vec::new();
+            let mut columns = vec!["Timestamp(ms)".to_string()];
+
+            for bit in 0..logger_config::N_ENUM {
+                let flag = 1 << bit;
+                if (self.mask & flag) != 0 {
+                    selected_data.push(bit);
+                    columns.push(logger_config::LOG_MASK_NAMES[bit].to_string());
                 }
             }
-            let _ = writer.write_record(&row);
-        }
 
-        let _ = writer.flush();
-        println!("INFO: Firmware Logger has been saved on: {}", file_path);    
+            let file = fs::File::create(&file_path).unwrap();
+            let mut writer = csv::Writer::from_writer(file);
+            let _ = writer.write_record(&columns);
+
+            for data_line in collected_logs {
+                let mut row = vec![data_line.dt.to_string()];
+
+                for &idx in &selected_data {
+                    if idx < data_line.values.len() {
+                        let scale = logger_config::SCALE_OFFSET_MOTOR[idx][0];
+                        let offset = logger_config::SCALE_OFFSET_MOTOR[idx][1]; 
+                        let processed_value = (data_line.values[idx] as f64 * scale) + offset;
+                        row.push(format!("{:.4}", processed_value));
+                    }
+                }
+                let _ = writer.write_record(&row);
+            }
+
+            let _ = writer.flush();
+            println!("  [INFO] {} {}", "- Firmware Logger has been saved on:".bright_yellow(), file_path);    
+        }
     }
 }
--- rust_script/src/logger/mod.rs
+++ rust_script/src/logger/mod.rs
@@ -1,6 +1,7 @@
 
 use crate::board::rpi::LogEntry;
 use crate::board::rpi::Pico;
+use crate::config::logger_config;
 
 use std::sync::{Arc, Mutex};
 use std::sync::mpsc::Receiver;
@@ -8,5 +9,6 @@ use std::thread;
 use std::time::SystemTime;
 use std::time::UNIX_EPOCH;
 use std::fs;
+use colored::Colorize;
 
 pub mod fwlogger;
