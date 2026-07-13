d20326e8de728de452a209ac891a05fa5af6baf3
tutla53 <ayatullah_tutla@yahoo.com>
2026-07-13 17:41:39 +0700

    Refactor main: - Adding global SharedResources with OnceLock - Adding CLI Apps

--- rust_script/src/main.rs
+++ rust_script/src/main.rs
@@ -1,55 +1,113 @@
+mod apps;
 mod board;
 mod basic_function;
 mod logger;
 mod config;
 
+use crate::apps::editor::initialized_editor;
+use crate::basic_function::utility::safe_exit;
 use crate::board::rpi::Pico;
 use crate::basic_function::move_motor::Motor;
-use crate::basic_function::conversion::Position;
-use crate::basic_function::conversion::Speed;
-use crate::basic_function::conversion::Acceleration;
 use crate::logger::fwlogger::Logger;
-use crate::basic_function::utility::wait_ms;
+use crate::apps::cli::ReplCli;
+use crate::apps::cli::ReplCommands;
+use crate::apps::command_handler as CommandHandler;
 
-use std::time::Duration;
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, Mutex, OnceLock};
+use clap::Parser;
+use rustyline::error::ReadlineError;
+use colored::Colorize;
+
+const MOTOR_ID: u8 = 0;
+pub static SHARED: OnceLock<SharedResources> = OnceLock::new();
 
 include!(concat!(env!("OUT_DIR"), "/generated_commands.rs"));
 
+pub struct SharedResources {
+    pico: Arc<Mutex<Pico>>,
+    m0: Arc<Mutex<Motor>>, 
+    logger: Arc<Mutex<Logger>>,
+    available_commands: Vec<String>,
+}
+
+impl SharedResources {
+    fn new(motor_id: u8) -> Result<Self, Box<dyn std::error::Error>> {
+        let (pico, log_rx) = Pico::new(env!("CONFIG_FILE"))?;
+        let mut available_commands: Vec<String> = pico.cmd_definitions.keys().cloned().collect();
+        available_commands.sort();
+
+        let shared_pico = Arc::new(Mutex::new(pico));
+        let m0 = Motor::new(Arc::clone(&shared_pico), motor_id);
+        let logger = Logger::new(Arc::clone(&shared_pico), log_rx, m0.motor_id);
+
+        let shared_m0 = Arc::new(Mutex::new(m0));
+        let shared_logger = Arc::new(Mutex::new(logger));
+        
+        Ok(SharedResources {
+            pico: shared_pico, 
+            m0: shared_m0, 
+            logger: shared_logger,
+            available_commands,
+            }
+        )
+    }
+
+    fn initialized_resources(motor_id: u8) -> Result<(), Box<dyn std::error::Error>> {
+        let resources = Self::new(motor_id)?;
+        SHARED.set(resources).map_err(|_| "Shared resources already initialized!")?;
+        Ok(())
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let (pico, log_rx) = Pico::new(env!("CONFIG_FILE"))?;
-    let shared_pico = Arc::new(Mutex::new(pico));
-    
-    let motor_id = 0;
-    let m0 = Motor::new(Arc::clone(&shared_pico), motor_id);
-    let mut logger = Logger::new(Arc::clone(&shared_pico), log_rx, m0.motor_id);
 
-    let current_pos = m0.get_motor_pos()?;
-    println!("Initial Pos: {} count, {:.2} rotation", current_pos.count, current_pos.rotation);
+    let _ = SharedResources::initialized_resources(MOTOR_ID)?;
+    let mut editor = initialized_editor()?;
 
-    let target_rotation = if current_pos.rotation != 0.0 { 0.0 } else { 20.0 };
-    let log_mask = 5;
-    let time_sampling = 1;
+    let prompt = format!("{}> ", "rp2040".green());
+    println!("Type 'move' to run the function, or 'exit' to quit.");
 
-    m0.clear_motor_event();
+    loop {
+        let readline = editor.readline(&prompt);
+        match readline {
+            Ok(line) => {
+                let trimmed = line.trim();
+                if trimmed.is_empty() { continue; }
+                
+                let _ = editor.add_history_entry(trimmed);
+                let args: Vec<&str> = trimmed.split_whitespace().collect();
 
-    logger.start(log_mask, time_sampling);
-    wait_ms(300);
-
-    m0.move_motor_pos_trapezoid(
-        Position::from_rotation(target_rotation), 
-        Speed::from_rpm(1000.0), 
-        Acceleration::from_cps_sq(10_000),
-    );
-    
-    m0.wait_move_done(Duration::from_secs(20))?;
-    
-    wait_ms(300);
-    logger.stop();
-    m0.stop_motor();
-
-    let current_pos = m0.get_motor_pos()?;
-    println!("Final Pos: {} count, {:.2} rotation", current_pos.count, current_pos.rotation);
+                match ReplCli::try_parse_from(&args) {
+                    Ok(cli) => {
+                        match cli.command {
+                            ReplCommands::Move => {
+                                CommandHandler::move_motor_handler()?;
+                            }
+                            ReplCommands::Dev { all, command_name, args } => {
+                                CommandHandler::api_handler(all, command_name, args);
+                            },
+                            ReplCommands::Exit => {
+                                println!("Exiting loop...");
+                                break;
+                            }
+                        }
+                    }
+                    Err(clap_error) => {
+                        println!("{}", clap_error);
+                    }
+                }
+            }
+            Err(ReadlineError::Interrupted) | Err(ReadlineError::Eof) => {
+                safe_exit();
+                println!("\nSession terminated.");
+                break;
+            }
+            Err(err) => {
+                println!("Error reading line: {:?}", err);
+                break;
+            }
+        }
+    }
     
     Ok(())
 }
