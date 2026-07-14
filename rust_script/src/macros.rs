20707f97854d23c99f9468ae5ea1db2d42d86c53
tutla53 <ayatullah_tutla@yahoo.com>
2026-07-14 18:30:50 +0700

    creating the macro with_lock! to accomodate clean calling SharedResources

---
+++ rust_script/src/macros.rs
@@ -0,0 +1,44 @@
+// src/macros.rs
+
+pub trait IntoResult {
+    type OkType;
+    type ErrType;
+    fn into_result(self) -> Result<Self::OkType, Self::ErrType>;
+}
+
+impl<T, E> IntoResult for Result<T, E> {
+    type OkType = T;
+    type ErrType = E;
+    fn into_result(self) -> Result<T, E> { self }
+}
+
+impl IntoResult for () {
+    type OkType = ();
+    type ErrType = &'static str;
+    fn into_result(self) -> Result<(), &'static str> { Ok(()) }
+}
+
+#[macro_export]
+macro_rules! with_lock {
+    ($resources:expr, $field:ident . $method:ident ( $($args:expr),* $(,)? )) => {
+        match $resources.$field.lock() {
+            #[allow(unused_mut)]
+            Ok(mut target) => {
+                use $crate::macros::IntoResult;
+                match target.$method($($args),*).into_result() {
+                    Ok(val) => Ok(val),
+                    Err(e) => Err(Box::<dyn std::error::Error>::from(e)),
+                }
+            },
+            Err(_) => Err(Box::<dyn std::error::Error>::from("Mutex poisoned")),
+        }
+    };
+
+    ($resources:expr, $field:ident => |$target:ident| $body:expr) => {
+        match $resources.$field.lock() {
+            #[allow(unused_mut)]
+            Ok(mut $target) => Ok($body),
+            Err(_) => Err(Box::<dyn std::error::Error>::from("Mutex poisoned")),
+        }
+    };
+}
