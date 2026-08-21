cd0f9573acf4a8b54933430a3a910344c5c683b4
Tutla Ayatullah <tutla.ayatullah@formulatrix.com>
2026-08-21 15:06:34 +0700

    Change safe_exit from stop_motor to disable motor

--- rust_script/src/basic_function/utility.rs
+++ rust_script/src/basic_function/utility.rs
@@ -9,13 +9,13 @@ pub fn wait_ms(duration_ms: u64) {
 pub fn safe_exit(pico: Arc<Mutex<Pico>>, m0: Arc<Mutex<Motor>>, logger: Arc<Mutex<Logger>>) {
     println!("\n{}", "Perfroming Safe Exit!".bright_green().bold());
 
-    let is_sim = run_with_lock!(pico => is_sim_mode()).unwrap_or(false);
+    let is_sim = try_lock!(pico => is_sim_mode()).unwrap_or(false);
 
     if !is_sim {
-        println!("- Stopping Motor");
-        let _ = run_with_lock!(m0 => stop_motor());
+        println!("- Disabling Motor");
+        let _ = try_lock!(m0 => disable());
 
         println!("- Stopping Logger");
-        let _ = run_with_lock!(logger => stop());
+        let _ = try_lock!(logger => stop());
     }
 }
