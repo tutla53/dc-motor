a25089b824ccb4bcd52786100fd25e40bb28df4b
Tutla Ayatullah <tutla.ayatullah@formulatrix.com>
2026-09-04 17:39:55 +0700

    Implementing script to the new update

--- rust_script/src/program/script.rs
+++ rust_script/src/program/script.rs
@@ -2,7 +2,7 @@ use super::*;
 
 // TODO: Add the guidline to create the custom script
 
-pub fn enable_m0() -> Result<(), Box<dyn std::error::Error>> {
+pub fn enable_motor() -> Result<(), Box<dyn std::error::Error>> {
     let shared = SHARED.get().expect("Shared resources not initialized!");
 
     let is_enabled = try_lock!(shared.m0 => is_enabled())??;
@@ -41,15 +41,9 @@ pub fn open_loop(percent: f64) -> Result<(), Box<dyn std::error::Error>> {
         Ok(())
     })();
 
-    let motor_stop_result = try_lock!(shared.m0 => stop_motor());
-    let logger_stop_result = try_lock!(shared.logger => stop());
-
-    move_status?;
-    motor_stop_result??;
+    let (log_dir, file_dir) = finalize_motor_routine(&shared.m0, &shared.logger, move_status)?;
 
     /* ---------- Plot Firmware Log ---------- */
-    let (log_dir, file_dir) = logger_stop_result??;
-
     let csv_log = CsvProcessing::extract_information(
         &file_dir,
         TIMESTAMP_INDEX,
@@ -79,13 +73,21 @@ pub fn pos_trapezoid_move(
 
     /* ---------- Gathering Motor Info ---------- */
     let initial_pos = try_lock!(shared.m0 => get_motor_pos())??;
-    println!(
-        "Initial Pos: {} count, {:.2} rotation",
-        initial_pos.count, initial_pos.rotation
-    );
     let pid_speed_config = try_lock!(shared.m0 => get_pid_motor_speed())??;
     let pid_pos_config = try_lock!(shared.m0 => get_pid_motor_pos())??;
     let max_speed_pps = try_lock!(shared.m0 => get_motor_max_speed())??;
+
+    /* ---------- Estimate Move Time ---------- */
+    let timeout_ms = get_move_timeout_ms(
+        &initial_pos,
+        &Position::from_rotation(target_rotation),
+        &Speed::from_rpm(speed_rpm),
+        &Acceleration::from_cps_sq(acc_cps2),
+        max_speed_pps as i32,
+    )?;
+
+    /* ---------- Display Motor Info ---------- */
+    println!("  [INFO] - Initial Pos: {initial_pos}");
 
     /* ---------- Move Motor ---------- */
     try_lock!(shared.m0 => clear_motor_event())?;
@@ -102,30 +104,19 @@ pub fn pos_trapezoid_move(
             )
         )??;
 
-        if let Err(e) = try_lock!(shared.m0 => wait_move_done(Duration::from_secs(20)))? {
-            println!("{}", e);
-        }
+        try_lock!(shared.m0 => wait_move_done(Duration::from_millis(timeout_ms)))??;
         wait_ms(300);
 
         Ok(())
     })();
 
-    let motor_stop_result = try_lock!(shared.m0 => stop_motor());
-    let logger_stop_result = try_lock!(shared.logger => stop());
-
-    move_status?;
-    motor_stop_result??;
+    let (log_dir, file_dir) = finalize_motor_routine(&shared.m0, &shared.logger, move_status)?;
 
     /* ---------- Get Motor Pos ---------- */
     let current_pos = try_lock!(shared.m0 => get_motor_pos())??;
-    println!(
-        "Final Pos: {} count, {:.2} rotation",
-        current_pos.count, current_pos.rotation
-    );
+    println!("  [INFO] - Final Pos: {current_pos}");
 
     /* ---------- Plot Firmware Log ---------- */
-    let (log_dir, file_dir) = logger_stop_result??;
-
     let csv_log = CsvProcessing::extract_information(
         &file_dir,
         TIMESTAMP_INDEX,
@@ -158,13 +149,21 @@ pub fn pos_step_move(target_rotation: f64) -> Result<(), Box<dyn std::error::Err
 
     /* ---------- Gathering Motor Info ---------- */
     let initial_pos = try_lock!(shared.m0 => get_motor_pos())??;
-    println!(
-        "Initial Pos: {} count, {:.2} rotation",
-        initial_pos.count, initial_pos.rotation
-    );
     let pid_speed_config = try_lock!(shared.m0 => get_pid_motor_speed())??;
     let pid_pos_config = try_lock!(shared.m0 => get_pid_motor_pos())??;
     let max_speed_pps = try_lock!(shared.m0 => get_motor_max_speed())??;
+
+    /* ---------- Estimate Move Time ---------- */
+    let timeout_ms = get_move_timeout_ms(
+        &initial_pos,
+        &Position::from_rotation(target_rotation),
+        &Speed::from_cps(max_speed_pps as i32),
+        &Acceleration::from_cps_sq(1_000_000), // TODO: Move this value to the motor config
+        max_speed_pps as i32,
+    )?;
+
+    /* ---------- Display Motor Info ---------- */
+    println!("  [INFO] - Initial Pos: {initial_pos}");
 
     /* ---------- Move Motor ---------- */
     try_lock!(shared.m0 => clear_motor_event())?;
@@ -176,30 +175,19 @@ pub fn pos_step_move(target_rotation: f64) -> Result<(), Box<dyn std::error::Err
             shared.m0 => move_motor_pos_step(Position::from_rotation(target_rotation))
         )??;
 
-        if let Err(e) = try_lock!(shared.m0 => wait_move_done(Duration::from_secs(20)))? {
-            println!("{}", e);
-        }
+        try_lock!(shared.m0 => wait_move_done(Duration::from_millis(timeout_ms)))??;
         wait_ms(300);
 
         Ok(())
     })();
 
-    let motor_stop_result = try_lock!(shared.m0 => stop_motor());
-    let logger_stop_result = try_lock!(shared.logger => stop());
-
-    move_status?;
-    motor_stop_result??;
+    let (log_dir, file_dir) = finalize_motor_routine(&shared.m0, &shared.logger, move_status)?;
 
     /* ---------- Get Motor Pos ---------- */
     let current_pos = try_lock!(shared.m0 => get_motor_pos())??;
-    println!(
-        "Final Pos: {} count, {:.2} rotation",
-        current_pos.count, current_pos.rotation
-    );
+    println!("  [INFO] - Final Pos: {current_pos}");
 
     /* ---------- Plot Firmware Log ---------- */
-    let (log_dir, file_dir) = logger_stop_result??;
-
     let csv_log = CsvProcessing::extract_information(
         &file_dir,
         TIMESTAMP_INDEX,
@@ -246,15 +234,9 @@ pub fn speed_move(target_speed: f64) -> Result<(), Box<dyn std::error::Error>> {
         Ok(())
     })();
 
-    let motor_stop_result = try_lock!(shared.m0 => stop_motor());
-    let logger_stop_result = try_lock!(shared.logger => stop());
-
-    move_status?;
-    motor_stop_result??;
+    let (log_dir, file_dir) = finalize_motor_routine(&shared.m0, &shared.logger, move_status)?;
 
     /* ---------- Plot Firmware Log ---------- */
-    let (log_dir, file_dir) = logger_stop_result??;
-
     let csv_log = CsvProcessing::extract_information(
         &file_dir,
         TIMESTAMP_INDEX,
